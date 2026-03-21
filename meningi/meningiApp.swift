f9113b824861bb5fe3cc4a3ca08cca9f0a80acd8
Alex Kolesnikov <alexkolesnikov2010@gmail.com>
2026-03-21 09:27:28 +0000

    Initial Commit

---
+++ meningi/meningiApp.swift
@@ -0,0 +1,17 @@
+//
+//  meningiApp.swift
+//  meningi
+//
+//  Created by Alex Kolesnikov on 21/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct meningiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ meningiTests/meningiTests.swift
@@ -0,0 +1,17 @@
+//
+//  meningiTests.swift
+//  meningiTests
+//
+//  Created by Alex Kolesnikov on 21/03/2026.
+//
+
+import Testing
+@testable import meningi
+
+struct meningiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
